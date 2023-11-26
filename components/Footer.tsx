2380b46666eff4a32a00a2de9fd3c301c4494600
MECHREVO_GGPRO\AarynLu <hello@0x3f4.run>
2023-11-27 00:31:34 +0800

    add footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright © 2023 {config.author}</div>
+      <div className={styles.copyright}>Copyright © 2023 
+        <a href="https://0x3f4.run" target="_blank">{config.author}</a>
+      </div>
 
       <div className={styles.settings}>
         {hasMounted && (
